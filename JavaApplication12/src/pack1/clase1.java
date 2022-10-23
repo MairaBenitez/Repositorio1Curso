befe37e4813979fe348fbfaefeae26ff96abbddf
Benitez Maira Ayelen <mai.benitez2002@gmail.com>
2022-10-23 19:51:42 -0300

    commit quien sabe cuanto

--- JavaApplication12/src/pack1/clase1.java
+++ JavaApplication12/src/pack1/clase1.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package pack1;
 
 /**
  *
@@ -9,5 +10,4 @@
  */
 public class clase1 {
     int num;
-    
 }
